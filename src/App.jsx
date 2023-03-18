0b012b7a51e5a5bef226de581e1e205810a9b090
Sara Baqla <missatrox44@gmail.com>
2023-03-18 16:33:54 -0500

    created static ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,182 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import TableRotation from "./components/Sample";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isTrainee, setIsTrainee] = useState(false);
+
+  const handleStatusChange = () => {
+    setIsTrainee((prevState) => !prevState);
+  };
 
   return (
     <div className="App">
+      <header>
+        <h1>Crab City Server Rotation</h1>
+        <label for="name">Employee Name</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name Here"
+          required
+          // value={employeeName}
+          // onChange={handleChange}
+        />
+
+        <label>
+          <input
+            type="radio"
+            name="status"
+            value="trainee"
+            checked={isTrainee}
+            onChange={handleStatusChange}
+          />
+          Trainee
+        </label>
+
+        <button>Add</button>
+      </header>
+
+      {/* up next */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Next Server: Sylvester </h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <hr />
+
+      {/* small tops */}
+      <div>
+        <h3>Small Tops</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Total Tables</th>
+              <th>Assign</th>
+              <th>Skip</th>
+              <th>Break</th>
+              <th>Clock Out</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Finn</td>
+              <td>3</td>
+              <td>
+                <button>+</button>
+              </td>
+              <td>
+                <button>Skip</button>
+              </td>
+              <td>
+                <button>Break</button>
+              </td>
+              <td>
+                <button>Clock Out</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Chelsea</td>
+              <td>4</td>
+              <td>
+                <button>+</button>
+              </td>
+              <td>
+                <button>Skip</button>
+              </td>
+              <td>
+                <button>Break</button>
+              </td>
+              <td>
+                <button>Clock Out</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Boswell</td>
+              <td>4</td>
+              <td>
+                <button>+</button>
+              </td>
+              <td>
+                <button>Skip</button>
+              </td>
+              <td>
+                <button>Break</button>
+              </td>
+              <td>
+                <button>Clock Out</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Archimedes</td>
+              <td>5</td>
+              <td></td>
+              <td></td>
+              <td>
+                <button>Break Over</button>
+              </td>
+              <td>
+                <button>Clock Out</button>
+              </td>
+            </tr>
+            <tr className="clocked-out">
+              <td>Kyle</td>
+              <td>5</td>
+              <td>CLOCKED OUT</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <hr />
+      {/* big tops */}
+      <div>
+        <h3>Big Tops</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Total Tables</th>
+              <th>Assign</th>
+              <th>Skip</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Finn</td>
+              <td>0</td>
+              <td>
+                <button>+</button>
+              </td>
+              <td>
+                <button>Skip</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Chelsea</td>
+              <td>1</td>
+              <td>
+                <button>Ready for more tables</button>
+              </td>
+              <td>{/* <button>Skip</button> */}</td>
+            </tr>
+            <tr>
+              <td>Archimedes</td>
+              <td>1</td>
+              <td>{/* <button>Break Over</button> */}</td>
+              <td>{/* <button>Skip</button> */}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <hr />
+      <footer>
+        Created by{" "}
+        <a target="_blank" href="https://github.com/missatrox44">
+          S4R4
+        </a>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
