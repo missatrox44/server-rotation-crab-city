2c3e52aec17cba295ae129f65ae2029202ec074b
A <andcooke@gmail.com>
2023-05-06 10:58:08 -0500

    made employee employee entry a form, with add button a submit input

--- src/App.jsx
+++ src/App.jsx
@@ -72,26 +72,29 @@ function App() {
         <h1 className="text-blue-600 text-6xl">Crab City Server Rotation</h1>
         <p className="text-xs mb-6">v.1.3.0</p>
 
-        <label for="name">Employee Name: </label>
-        <input
-          type="text"
-          name="name"
-          required
-          value={employeeName}
-          onChange={handleNameChange}
-          className="p-2 mx-2 rounded-lg"
-        />
-
-        <label className="mx-2">
+        <form>
+          <label for="name">Employee Name: </label>
           <input
-            type="checkbox"
-            checked={isTrainee}
-            onChange={handleTraineeChange}
+            type="text"
+            name="name"
+            required
+            value={employeeName}
+            onChange={handleNameChange}
+            className="p-2 mx-2 rounded-lg text-black"
           />
-          Training
-        </label>
 
-        <AddBtn onClick={handleAddEmployee} />
+          <label className="mx-2">
+            <input
+              type="checkbox"
+              checked={isTrainee}
+              onChange={handleTraineeChange}
+            />
+            Training
+          </label>
+
+          <AddBtn onClick={handleAddEmployee} />
+        </form>
+        
       </header>
 
       {/* up next */}
