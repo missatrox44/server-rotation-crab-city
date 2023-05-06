c26d37ef123d0351fbc63df3256da60f8eee4aa0
A <andcooke@gmail.com>
2023-05-06 15:16:31 -0500

    changed employee ID to date.now

--- src/components/ClockOutModal.jsx
+++ src/components/ClockOutModal.jsx
@@ -28,10 +28,10 @@ export default function ClockOutModal({ employee, employees, setEmployees, setIs
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
-                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                  <h3 className="text-base font-bold leading-10 text-2xl text-gray-900" id="modal-title">{employee.employeeName}</h3>
+                <div className="mt-3 text-center w-full sm:ml-4 sm:mt-0 sm:text-left">
+                  <h3 className="text-base font-bold leading-10 text-4xl text-gray-900" id="modal-title">{employee.employeeName}</h3>
                   <div className="mt-2">
-                    <p className="text-sm text-gray-500 text-lg">Are you sure you want to clock out?</p>
+                    <p className="text-sm text-gray-500 my-5 text-xl">Are you sure you want to clock out?</p>
                   </div>
                 </div>
               </div>
@@ -39,14 +39,14 @@ export default function ClockOutModal({ employee, employees, setEmployees, setIs
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button 
                 type="button" 
-                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                className="inline-flex w-full justify-center rounded-full bg-red-600 px-3 py-2.5 px-5 text-md font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                 onClick={handleClockOut}
               >
                 Clock Out
               </button>
               <button 
                 type="button" 
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                className="mt-3 mx-2 inline-flex w-full justify-center rounded-md bg-white px-3 py-2.5 text-md font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                 onClick={() => setIsModalVisible(false)}
               >
                 Cancel
