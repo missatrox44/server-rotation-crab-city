a6ca9d31a5adfaec8f9b9a60b5e57e8133d4b7a6
Sara Baqla <missatrox44@gmail.com>
2023-07-27 19:27:22 -0500

    added error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import { useState, useEffect, useRef } from "react";
 import { ref, onValue } from "firebase/database";
 import "./App.css";
-import { db } from './utils/firebase';
+import { db } from "./utils/firebase";
 import Footer from "./components/Footer";
 import HeaderForm from "./components/HeaderForm";
 import NextServer from "./components/NextServer";
 import EmployeeTable from "./components/EmployeeTable";
 
 function App() {
-
-  const [employees, setEmployees] = useState({employeeData: []});
+  const [employees, setEmployees] = useState({ employeeData: [] });
   const [bigTopEmployees, setBigTopEmployees] = useState([]);
   const [breakEmployees, setBreakEmployees] = useState([]);
   const [nextServerIndex, setNextServerIndex] = useState(0);
+  const [error, setError] = useState(null);
 
   let lastAction = useRef({});
+
+  // Error reset function
+  const resetError = () => {
+    setError(null);
+  };
 
   const handleUndo = () => {
     if (!lastAction.current.action) {
       return;
     }
-    const {action, employee, currentEmployeeList, currentBigTopEmployeeList, currentBreakEmployeeList} = lastAction.current;
+    const {
+      action,
+      employee,
+      currentEmployeeList,
+      currentBigTopEmployeeList,
+      currentBreakEmployeeList,
+    } = lastAction.current;
     if (action === "big top") {
       employee.bigTopTotal = employee.bigTopTotal - 1;
     }
@@ -28,29 +39,29 @@ function App() {
     setBigTopEmployees(currentBigTopEmployeeList);
     setBreakEmployees(currentBreakEmployeeList);
     lastAction.current = {};
-  }
-  
+  };
+
   useEffect(() => {
     getAllEmployees();
   }, []);
 
   const getAllEmployees = () => {
     try {
-      const getEmployees = ref(db, 'employees');
+      const getEmployees = ref(db, "employees");
       onValue(getEmployees, (snapshot) => {
-        let employeesArr = []; 
+        let employeesArr = [];
         snapshot.forEach((employeeSnapshot) => {
           const employeeKey = employeeSnapshot.key;
           const employeeData = employeeSnapshot.val();
-          employeesArr.push({"key": employeeKey, "value": employeeData});
-        })
-        setEmployees({employeeData: [...employeesArr]});
+          employeesArr.push({ key: employeeKey, value: employeeData });
+        });
+        setEmployees({ employeeData: [...employeesArr] });
       });
     } catch (error) {
       console.log(error);
+      setError(`Error fetching employees data: ${error.message}`);
     }
-  }
-  
+  };
 
   return (
     <div className="App">
@@ -58,16 +69,17 @@ function App() {
         <h1 className="text-blue-600 text-6xl">Crab City Server Rotation</h1>
         <p className="text-xs mb-6">v.1.3.0</p>
 
-        <HeaderForm employees={employees} setEmployees={setEmployees}/>
+        <HeaderForm employees={employees} setEmployees={setEmployees} />
       </header>
+      {error && <p className="error-message">{error}</p>}
 
-      {
-        (employees.employeeData.length > 0 || bigTopEmployees.length > 0 || breakEmployees.length > 0) && 
-      
+      {(employees.employeeData.length > 0 ||
+        bigTopEmployees.length > 0 ||
+        breakEmployees.length > 0) && (
         <div>
           <hr className="my-8" />
 
-            <NextServer employees={employees} nextServerIndex={nextServerIndex} />
+          <NextServer employees={employees} nextServerIndex={nextServerIndex} />
 
           <hr className="my-8" />
           <main className="overflow-x-auto">
@@ -84,12 +96,10 @@ function App() {
           </main>
 
           <hr className="my-8" />
-
         </div>
-      }
+      )}
 
       <Footer employees={employees} handleUndo={handleUndo} />
-
     </div>
   );
 }
